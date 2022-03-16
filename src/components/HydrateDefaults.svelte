7d772a3b4ee7a4d2e1293828d3b1f0a29165d09a
Rhys <36913739+ryceg@users.noreply.github.com>
2022-03-16 16:57:35 +1100

    extract

---
+++ src/components/HydrateDefaults.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Job } from '$lib/Job';
+	import { Person } from '$lib/Person';
+	import { random } from '$lib/random';
+	import { clients, jobs } from '$lib/stores';
+	import faker from '@faker-js/faker';
+	if (Object.keys($clients).length <= 1) {
+		for (let i = 0; i < 10; i++) {
+			const person = new Person({
+				firstName: faker.name.firstName(),
+				lastName: faker.name.lastName(),
+				address: faker.address.streetAddress(),
+				phone: faker.phone.phoneNumber(),
+				email: faker.internet.email(),
+				notes: faker.lorem.paragraph()
+			});
+			$clients[person.id] = person;
+			$clients = $clients;
+		}
+	}
+	if (Object.keys($jobs).length <= 1) {
+		for (let i = 0; i < 10; i++) {
+			const job = new Job({
+				client: random(Object.keys(clients)),
+				notes: faker.lorem.paragraph(),
+				title: faker.company.catchPhrase()
+			});
+			$jobs[job.id] = job;
+		}
+	}
+</script>
