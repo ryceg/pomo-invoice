a847cccbe2adb570068e2399b35659bd2bff8886
Rhys <36913739+ryceg@users.noreply.github.com>
2022-03-16 13:41:58 +1100

    extract consts

--- src/components/Pomodoro.svelte
+++ src/components/Pomodoro.svelte
@@ -1,23 +1,11 @@
-<script>
-	const minutesToSeconds = (minutes) => minutes * 60;
-	const secondsToMinutes = (seconds) => Math.floor(seconds / 60);
-	const padWithZeroes = (number) => number.toString().padStart(2, '0');
-	const State = { idle: 'idle', inProgress: 'in progress', resting: 'resting' };
-
-	const POMODORO_S = minutesToSeconds(25);
-	const LONG_BREAK_S = minutesToSeconds(20);
-	const SHORT_BREAK_S = minutesToSeconds(5);
+<script lang="ts">
+	import { formatTime, LONG_BREAK_S, POMODORO_S, SHORT_BREAK_S, State } from '$lib/consts';
+	import Button from './Button.svelte';
 
 	let currentState = State.idle;
 	let pomodoroTime = POMODORO_S;
 	let completedPomodoros = 0;
 	let interval;
-
-	function formatTime(timeInSeconds) {
-		const minutes = secondsToMinutes(timeInSeconds);
-		const remainingSeconds = timeInSeconds % 60;
-		return `${padWithZeroes(minutes)}:${padWithZeroes(remainingSeconds)}`;
-	}
 
 	function startPomodoro() {
 		setState(State.inProgress);
@@ -59,6 +47,9 @@
 		// TODO: Add some logic to prompt the user to write down the cause of the interruption.
 		idle();
 	}
+	function pausePomodoro() {
+		idle();
+	}
 
 	function idle() {
 		setState(State.idle);
@@ -66,11 +57,19 @@
 	}
 </script>
 
-<section>
-	<time>
-		{formatTime(pomodoroTime)}
+<section class="">
+	<time class="relative flex items-center justify-center w-full px-3 py-4 text-3xl font-bold">
+		{formatTime(pomodoroTime)} ({currentState})
 	</time>
-	<button on:click={startPomodoro} disabled={currentState !== State.idle}>Start</button>
-	<button on:click={cancelPomodoro} disabled={currentState !== State.inProgress}>Cancel</button>
+	<div class="relative flex items-center justify-center space-x-8">
+		<Button func={startPomodoro} disabled={currentState !== State.idle}>Start</Button>
+		{#if currentState !== 'idle'}
+			<Button func={cancelPomodoro} disabled={currentState !== State.inProgress}>Cancel</Button>
+		{/if}
+		{#if currentState === 'paused'}
+			<Button func={pausePomodoro} disabled={currentState !== State.inProgress}>Pause</Button>
+		{/if}
+	</div>
+
 	<!--button on:click={completePomodoro}>complete</button-->
 </section>
--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -3,3 +3,18 @@ export const defaults = {
   ratePerHour: ratePerHour,
   flatRate: 0
 }
+
+export const minutesToSeconds = (minutes: number): number => minutes * 60;
+export const secondsToMinutes = (seconds: number): number => Math.floor(seconds / 60);
+export const padWithZeroes = (number: number): string => number.toString().padStart(2, '0');
+export const State = { idle: 'idle', inProgress: 'in progress', resting: 'resting', pause: 'paused' };
+
+export const POMODORO_S = minutesToSeconds(25);
+export const LONG_BREAK_S = minutesToSeconds(20);
+export const SHORT_BREAK_S = minutesToSeconds(5);
+
+export function formatTime(timeInSeconds: number): string {
+  const minutes = secondsToMinutes(timeInSeconds);
+  const remainingSeconds = timeInSeconds % 60;
+  return `${padWithZeroes(minutes)}:${padWithZeroes(remainingSeconds)}`;
+}
