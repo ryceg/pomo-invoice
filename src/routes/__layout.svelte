0a85952fa6db6fffb6d53276e0bf2dc043efeef0
Rhys <36913739+ryceg@users.noreply.github.com>
2022-03-16 19:27:03 +1100

    add emoji

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,12 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link
+		rel="icon"
+		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍅</text></svg>"
+	/>
+</svelte:head>
 <div class="md:w-7/12 lg:6/12 relative w-10/12 py-20 mx-auto">
 	<slot />
 </div>
