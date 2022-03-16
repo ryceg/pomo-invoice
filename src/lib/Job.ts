670b36539d21cc4081a7813cff3a87f5e80d10d3
Rhys <36913739+ryceg@users.noreply.github.com>
2022-03-16 16:57:20 +1100

    add jobs

--- src/lib/Job.ts
+++ src/lib/Job.ts
@@ -12,10 +12,48 @@ export class Job {
     start: Date;
     end: Date | null;
   };
-  rate: number = ratePerHour;
+  ratePerHour: number = ratePerHour;
   flatRate = defaults.flatRate;
   expectedPomodoros = 2
   title = '';
   notes = '';
   isOpen = true;
 }
+
+interface JobField {
+  type: string
+  label: string
+  name: string
+  placeholder?: number
+}
+
+export const jobFields: JobField[] = [
+  {
+    type: 'text',
+    label: 'Title of Job',
+    name: 'title'
+  },
+  {
+    type: 'number',
+    label: 'Expected Pomodoros',
+    name: 'expectedPomodoros',
+    placeholder: defaults.expectedPomodoros
+  },
+  {
+    type: 'number',
+    label: 'Rate per hour',
+    name: 'ratePerHour',
+    placeholder: defaults.ratePerHour
+  },
+  {
+    type: 'number',
+    label: 'Flat Rate',
+    name: 'flatRate',
+    placeholder: defaults.flatRate
+  },
+  {
+    type: 'textfield',
+    label: 'Notes',
+    name: 'notes'
+  }
+]
