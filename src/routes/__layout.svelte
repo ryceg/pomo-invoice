55b1b74c34dfd0191eb353bc7cafb12c51771b47
Rhys <36913739+ryceg@users.noreply.github.com>
2022-04-12 19:54:26 +1000

    add auth stuff

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,18 @@
 <script>
+	import { loadClients, loadJobs, loadPomodoros, user } from '$lib/stores';
+	import { supabase } from '../../src/supabase';
 	import '../app.css';
+	import Auth from '../components/Auth.svelte';
 	import Menubar from '../components/Menubar.svelte';
+	user.set(supabase.auth.user());
+	supabase.auth.onAuthStateChange((_, session) => {
+		user.set(session?.user);
+		if (session?.user) {
+			loadClients();
+			loadJobs();
+			loadPomodoros();
+		}
+	});
 </script>
 
 <svelte:head>
@@ -9,7 +21,11 @@
 		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍅</text></svg>"
 	/>
 </svelte:head>
-<Menubar />
 <div class="md:w-7/12 lg:6/12 relative w-10/12 py-20 mx-auto">
+	{#if $user}
+		<Menubar />
+	{:else}
+		<Auth />
+	{/if}
 	<slot />
 </div>
